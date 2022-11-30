e8c107a6f6c5cf3dbd11292f0c0bee35ccc18a72
Julia Nikitina <joliejuly@gmail.com>
2022-11-30 13:27:20 +0300

    Create the FeedItem model

---
+++ EssentialDeveloperCourseProject/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialDeveloperCourseProject
+//
+//  Created by joliejuly on 30.11.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
