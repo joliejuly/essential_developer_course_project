4257bae8801b6c5d932ddf1c121ef74f3d2ca3ce
Julia Nikitina <joliejuly@gmail.com>
2022-11-30 13:21:44 +0300

    Initial Commit

---
+++ EssentialDeveloperCourseProject/EssentialDeveloperCourseProject.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeveloperCourseProject.h
+//  EssentialDeveloperCourseProject
+//
+//  Created by joliejuly on 30.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeveloperCourseProject.
+FOUNDATION_EXPORT double EssentialDeveloperCourseProjectVersionNumber;
+
+//! Project version string for EssentialDeveloperCourseProject.
+FOUNDATION_EXPORT const unsigned char EssentialDeveloperCourseProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeveloperCourseProject/PublicHeader.h>
+
+
