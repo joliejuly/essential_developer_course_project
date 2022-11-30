b697e8154a197bd0e025fa8715e4c0a1c0b4e3ce
Julia Nikitina <joliejuly@gmail.com>
2022-11-30 21:04:20 +0300

    Give a URL as a parameter in the constructor of `RemoteFeedLoader`

--- EssentialDeveloperCourseProjectTests/RemoteLoaderTests.swift
+++ EssentialDeveloperCourseProjectTests/RemoteLoaderTests.swift
@@ -9,9 +9,11 @@ import XCTest
 
 final class RemoteFeedLoader {
     private let client: HTTPClient
+    private let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
@@ -34,21 +36,25 @@ final class HTTPClientSpy: HTTPClient {
 final class RemoteLoaderTests: XCTestCase {
  
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "www.apple.com")!
         let client = HTTPClientSpy()
        
-        let _ = RemoteFeedLoader(client: client)
+        let _ = RemoteFeedLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        
+        let url = URL(string: "www.apple.com")!
         let client = HTTPClientSpy()
     
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(client: client, url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 
 }
